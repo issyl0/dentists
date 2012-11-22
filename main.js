954e7f1aaccddfc73f9e537abbd67debc7d60f87
issyl0 <isabell@issyl0.co.uk>
2012-11-22 19:15:03 +0000

    JavaScript file for the jQuery datepicker.

---
+++ main.js
@@ -0,0 +1,9 @@
+/* Isabell Long <isabell@issyl0.co.uk>, 2012.
+   JavaScript for the dentists transcription. */
+
+$(function() {
+        $( "#datepicker" ).datepicker({
+                dateFormat: "yy-mm-dd",
+                maxDate: "+0D"
+        });
+});
